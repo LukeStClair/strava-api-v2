3997c332d4aa93f2907e7eba9c0514d1f9da7277
Luke StClair <lukes@fb.com>
2013-05-03 11:18:07 -0700

    Rides now take parameters
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- lib/strava/v1/rides_request.rb
+++ lib/strava/v1/rides_request.rb
@@ -3,13 +3,29 @@ require 'strava/v1/models/ride'
 module Strava
   module V1
     module RidesRequest
+      RIDES_PARAMS_MAP = {
+        :club_id => :clubId,
+        :athlete_id => :athleteId,
+        :athlete_name => :athleteName,
+        :start_date => :startDate,
+        :end_date => :endDate,
+        :start_id => :startId,
+        :offset => :offset
+      }
+
       def rides_path
         'rides'
       end
 
-      def rides()
+      def rides(params = {})
+        final_params = {}
+        params.each_pair do |key, value|
+          raise RequestError.new if RIDES_PARAMS_MAP[key].nil?
+          final_params[RIDES_PARAMS_MAP[key]] = value
+        end
+
         all_rides = []
-        rides_array = self.fetch(rides_path(), nil)["rides"]
+        rides_array = self.fetch(rides_path(), final_params)["rides"]
         rides_array.each { |ride_json|
           all_rides.push(Ride.new(ride_json))
         }
--- test/rides_v1_test.rb
+++ test/rides_v1_test.rb
@@ -15,9 +15,24 @@ class RidesV1Test < Test::Unit::TestCase
 
   def test_rides_with_no_params
     Strava::V1::Connection.stubs(:get)
-    .with("/#{@s.rides_path}", {:query => nil})
+    .with("/#{@s.rides_path}", {:query => {}})
     .returns(stub(:code => 200, :parsed_response => (JSON.parse TestData.test_rides_no_params)))
     result = @s.rides()
     assert_equal(50, result.length)
+  end
+
+  def test_rides_with_invalid_param
+    assert_raise Strava::RequestError do
+      @s.rides({:made_up => "whatever"})
+    end
+  end
+
+  def test_rides_with_two_valid_params
+    Strava::V1::Connection.stubs(:get)
+    .with("/#{@s.rides_path}", {:query => {:startId => 77563, :offset => 50}})
+    .returns(stub(:code => 200, :parsed_response => (JSON.parse TestData.test_ride_start_id_and_offset)))
+    rides = @s.rides({:start_id => 77563, :offset => 50})
+
+    assert_equal(52127040, rides[0].id)
   end
 end
